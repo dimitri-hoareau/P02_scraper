557e4525045ffdb5299a9b124dedc2082861d399
dimitri-hoareau <dimitrihoareau2@gmail.com>
2021-02-06 14:49:39 +0100

    tranform list book_info in a dict, and add the script into a function

--- app.py
+++ app.py
@@ -1,55 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
+import csv 
 
-url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+def get_book_informations(book_url) :
+    # url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
+    url = book_url
+    response = requests.get(url)
 
-response = requests.get(url)
+    book_info = {}
 
-book_info = []
-book_info_heading = []
-book_info = []
+    if response.ok :
+        soup = BeautifulSoup(response.text, 'lxml')
 
-if response.ok :
-    soup = BeautifulSoup(response.text, 'lxml')
+        book_page_url = response.url
+        book_info["product_page_url"] = book_page_url
 
-    book_page_url = response.url
-    book_info_heading.append("product_page_url")
-    book_info.append(book_page_url)
+        book_description = soup.find_all("p")[-1].text.replace(",", " ")
+        book_info["product_description"] = book_description
 
-    book_description = soup.find_all("p")[-1].text.replace(",", " ")
-    book_info_heading.append("product_description")
-    book_info.append(book_description)
+        book_title = soup.find('h1').text
+        book_info["title"] = book_title
 
-    title = soup.find('h1')
-    book_title = title.text
-    book_info_heading.append("title")
-    book_info.append(book_title)
+        image = soup.find("img")
+        image_url = 'http://books.toscrape.com' + image['src']
+        book_image_url = image_url.replace('../..','')
+        book_info["image_url"] = book_image_url
 
-    image = soup.find("img")
-    image_url = 'http://books.toscrape.com' + image['src']
-    book_image_url = image_url.replace('../..','')
-    book_info_heading.append("image_url")
-    book_info.append(book_image_url)
+        book_info_heading = ["universal_product_code(UPC)","category","price_excluding_tax","price_including_tax","tax","number_availaible","review_rating"]
+        table_info = soup.find_all('td')
+        book_info_extra = []
+        for info in table_info :
+            book_info_extra.append(info.text)
 
-    book_info_heading += ["universal_product_code(UPC)","category","price_excluding_tax","price_including_tax","number_availaible","review_rating"]
+        extra_book_info = dict(zip(book_info_heading, book_info_extra))
 
-    # table_info_heading = soup.find_all('th')
-    # for heading in table_info_heading :
-    #     book_info_heading.append(heading.text)
-
-    table_info = soup.find_all('td')
-    for info in table_info :
-        book_info.append(info.text)
-
-    del book_info[8]
-
-    with open("info.txt", "w") as infile :
-        with open("book_info.csv", "w") as outfile :
-            for heading in book_info_heading :
-                outfile.write(heading + ",")
-            outfile.write("\n")
-            for info in book_info :
-                outfile.write(info + ",")
+        book_info.update(extra_book_info)
+        book_info.pop("tax")
 
 
+        with open('book_info.csv', 'w') as f:  
+            w = csv.DictWriter(f, book_info.keys())
+            w.writeheader()
+            w.writerow(book_info)
+
+
+
+
+get_book_informations("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
